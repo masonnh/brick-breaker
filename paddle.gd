526a732823bc8f9b1e1885264c9d507b94b6f599
Mason Hunter <masonlizard@gmail.com>
2026-09-08 17:30:04 -0400

    paddle movement

---
+++ paddle.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+const SPEED = 600
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * SPEED
+	move_and_slide()
