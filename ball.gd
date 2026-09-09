a0fcef9f6dd45d8f0d05c4feb32dc451b2bf01cf
Mason Hunter <masonlizard@gmail.com>
2026-09-09 17:03:06 -0400

    bounce ball

--- ball.gd
+++ ball.gd
@@ -1,14 +1,32 @@
-extends Area2D
+extends CharacterBody2D
 
 const SPEED = 500
-var velocity
+const BOUNCINESS = 1.0
+var screen_size: Vector2
+var direction: Vector2 = Vector2(1, 1).normalized()
 
 func _ready() -> void:
-	velocity = SPEED * getRandomStartDirection()
+	direction = getRandomStartDirection()
+	velocity = direction * SPEED
+	screen_size = get_viewport_rect().size
 
 func getRandomStartDirection() -> Vector2:
-	var dir = Vector2(randf_range(-1.0, 1.0), randf_range(0, 1.0)) # Always starts downwards
+	var dir := Vector2(randf_range(-0.5, 0.5), randf_range(0, 1.0)) # Always starts downwards
 	return dir.normalized()
 
 func _physics_process(delta: float) -> void:
-	position += velocity * delta
+	# Move the ball
+	#velocity = direction * SPEED
+	position.x = clamp(position.x, 30, screen_size.x - 30)
+	position.y = clamp(position.y, 30, screen_size.y + 35)
+	
+	# Bounce off paddle and bricks
+	var collision_info := move_and_collide(velocity * delta)
+	if collision_info:
+		#var collider := collision_info.get_collider()
+		var normal := collision_info.get_normal()
+		velocity = velocity.bounce(normal) * BOUNCINESS
+	
+	if position.y > screen_size.y:
+		# Respawn ball
+		pass
--- paddle.gd
+++ paddle.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
 const SPEED = 600
+var screen_size: Vector2
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
 
 func _physics_process(delta: float) -> void:
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("ui_left", "ui_right")
 	velocity.x = direction * SPEED
 	velocity.y = 0
-	move_and_slide()
+	position += velocity * delta
+	position.x = clamp(position.x, 125, screen_size.x - 125)
